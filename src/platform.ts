0b52319e1b6ce2e0e62a9c6ec04127405ee8193c
dj-nic <70818245+dj-nic@users.noreply.github.com>
2025-12-01 23:56:00 +0100

    Refactor Dehumidifier category assignment for HomeKit compatibility

--- src/platform.ts
+++ src/platform.ts
@@ -200,8 +200,8 @@ export class DreoPlatform implements DynamicPlatformPlugin {
           new HumidifierAccessory(this, accessory, state);
           break;
         case 'DR-HDH':
-          // Dehumidifier
-          accessory.category = this.api.hap.Categories.DEHUMIDIFIER ?? this.api.hap.Categories.AIR_PURIFIER;
+          // Dehumidifier (HomeKit lacks a dedicated enum, reuse AIR_PURIFIER so it groups correctly)
+          accessory.category = this.api.hap.Categories.AIR_PURIFIER;
           new DehumidifierAccessory(this, accessory, state);
           break;
 
